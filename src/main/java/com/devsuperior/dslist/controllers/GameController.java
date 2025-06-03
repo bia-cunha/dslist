4c7ea0d8e9fdfdcfd0f8a418021f2417a38a01d5
Bianca Cunha <bcunha1202@gmail.com>
2025-06-03 16:05:03 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -2,7 +2,6 @@ package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +26,10 @@ public class GameController {
 
     @GetMapping
     public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 
-
 }
+
+
+
